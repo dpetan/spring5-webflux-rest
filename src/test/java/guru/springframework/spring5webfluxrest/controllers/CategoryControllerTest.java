faeba5d6b808d93ca23981f4dc1955a63f36321d
Danijel Petanovic <danijel.petanovic@ericsson.com>
2019-09-01 00:55:37 +0200

    Update Category with PUT done.

--- src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -61,4 +61,17 @@ public class CategoryControllerTest {
                      .exchange()
                      .expectStatus().isCreated();
     }
+
+    @Test
+    public void updateCategory() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                  .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("Some Cat").build());
+
+        webTestClient.put().uri("/api/v1/categories/asdffda")
+                     .body(catToUpdateMono, Category.class)
+                     .exchange()
+                     .expectStatus().isOk();
+    }
 }
